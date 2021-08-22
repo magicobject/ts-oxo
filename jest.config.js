df4ba13d49ec8c47f79973863f1bb294da5dd5e7
Greg Wright <gregw@kainos.com>
2021-08-22 20:49:27 +0100

    add/update config files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['<rootDir>/dist/*'],
-    reporters: ['default'],
-    testTimeout: 20000
-  }
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
